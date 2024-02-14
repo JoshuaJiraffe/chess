e28d9bcfc170fe1b93f20404060881be9843d23c
Joshua Nielsen <joshuadinielsen@gmail.com>
2024-02-13 21:24:36 -0700

    Made Castling and EnPassant separate classes

--- shared/src/main/java/chess/CastlingMove.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-public class CastlingMove extends ChessMove
-{
-    public CastlingMove(ChessPosition startPosition, ChessPosition endPosition)
-    {
-        super(startPosition, endPosition, null);
-    }
-}
