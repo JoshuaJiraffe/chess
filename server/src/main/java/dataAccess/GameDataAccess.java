c86846fd2906ac5b44d6b791b2cc7db1c112c9f0
Joshua Nielsen <joshuadinielsen@gmail.com>
2024-03-01 00:15:26 -0700

    Wrote Memory Game Data Access Methods

--- server/src/main/java/dataAccess/GameDataAccess.java
+++ server/src/main/java/dataAccess/GameDataAccess.java
@@ -1,5 +1,6 @@
 package dataAccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 import java.util.Collection;
@@ -7,10 +8,10 @@ import java.util.Collection;
 public interface GameDataAccess
 {
     void clear() throws DataAccessException;
-    GameData createGame(GameData game) throws DataAccessException;
+    GameData createGame(String gameName) throws DataAccessException;
     Collection<GameData> listGames() throws DataAccessException;
-    boolean updateGame(int gameID, GameData game) throws DataAccessException;
-    boolean joinGame(String username, String playerColor, int gameID) throws DataAccessException;
+    boolean updateGame(int gameID, ChessGame game) throws DataAccessException;
+    GameData joinGame(String username, ChessGame.TeamColor playerColor, int gameID) throws DataAccessException;
     boolean deleteGame(int gameID) throws DataAccessException;
     int getSize() throws DataAccessException;
 }
