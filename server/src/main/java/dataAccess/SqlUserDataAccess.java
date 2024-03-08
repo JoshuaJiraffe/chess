d0428e79c9e451fbb3a3316951c476e6eb8ea33c
Joshua Nielsen <joshuadinielsen@gmail.com>
2024-03-07 22:25:17 -0700

    Finished sql user dao

--- server/src/main/java/dataAccess/SqlUserDataAccess.java
+++ server/src/main/java/dataAccess/SqlUserDataAccess.java
@@ -1,6 +1,11 @@
 package dataAccess;
 
+import com.google.gson.Gson;
+import model.AuthData;
 import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.SQLException;
 
 public class SqlUserDataAccess extends SqlDataAccess implements UserDataAccess
 {
@@ -18,24 +23,78 @@ public class SqlUserDataAccess extends SqlDataAccess implements UserDataAccess
     @Override
     public UserData createUser(UserData user) throws DataAccessException
     {
-        return null;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashPass = encoder.encode(user.password());
+        if(userExists(user))
+            throw new DataAccessException("Error: already taken", 403);
+        var statement = "INSERT INTO user (username, password, email, json) VALUES (?, ?, ?, ?)";
+        var json = new Gson().toJson(new UserData(user.username(), hashPass, user.email()));
+        executeUpdate(statement, user.username(), hashPass, user.email(), json);
+
+        return user;
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException
     {
-        return null;
+        try(var conn = DatabaseManager.getConnection())
+        {
+            var statement = "SELECT json FROM auth WHERE username=?";
+            try(var ps = conn.prepareStatement(statement))
+            {
+                ps.setString(1, username);
+                try(var rs = ps.executeQuery())
+                {
+                    if(rs.next())
+                    {
+                        String json = rs.getString(1);
+                        return new Gson().fromJson(json, UserData.class);
+                    }
+                    else
+                        throw new DataAccessException("Error: bad request", 400);
+                }
+            }
+        } catch (SQLException e)
+        {
+            throw new DataAccessException(e.getMessage(), 500);
+        }
     }
 
     @Override
     public boolean verifyUser(String username, String password) throws DataAccessException
     {
-        return false;
+        try(var conn = DatabaseManager.getConnection())
+        {
+            var statement = "SELECT json FROM user WHERE username=?";
+            try(var ps = conn.prepareStatement(statement))
+            {
+                ps.setString(1, username);
+                try(var rs = ps.executeQuery())
+                {
+                    if(rs.next())
+                    {
+                        String json = rs.getString(1);
+                        UserData user = new Gson().fromJson(json, UserData.class);
+                        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+                        return encoder.matches(password, user.password());
+                    }
+                    else
+                        throw new DataAccessException("Error: bad request", 400);
+                }
+            }
+        } catch (SQLException e)
+        {
+            throw new DataAccessException(e.getMessage(), 500);
+        }
     }
 
-    public boolean userExists(UserData user)
+    public boolean userExists(UserData user) throws DataAccessException
     {
-        var statement = "SELECT COUNT(*) FROM user"
+        var statement = "SELECT COUNT(*) FROM user WHERE username = ? OR email = ?";
+        if (executeUpdate(statement, user.username(), user.email()) > 0)
+            return true;
+        return false;
+
     }
 
     @Override
